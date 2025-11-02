4433786c0ecc4907bf0ae01e2f3e032df14c4476
sandeepreddykamidi <116157827+sandeepreddykamidi@users.noreply.github.com>
2025-11-02 21:16:33 +0530

    Change staging location to 'india' for file upload

--- ingest-api-data.py
+++ ingest-api-data.py
@@ -73,7 +73,7 @@ def get_air_quality_data(api_key, limit):
 
             logging.info(f'File Written to local disk with name: {file_name}')
             
-            stg_location = '@dev_db.stage_sch.raw_stg/'+today_string+'/'
+            stg_location = '@dev_db.stage_sch.raw_stg/india/'+today_string+'/'
             sf_session = snowpark_basic_auth()
             
             logging.info(f'Placing the file, the file name is {file_name} and stage location is {stg_location}')
